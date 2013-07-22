4864f81393c474575fc462841fb72d520d218d1b
William Reveal <bill@revealitconsulting.com>
2013-07-22 17:35:43 -0500

    attempting to do a submodule for the Library

--- app/Framework/Library/DbAdminInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Wer\Framework\Interfaces;
-
-interface DbAdminInterface
-{
-    public function showColumns($table_name);
-    public function showTables();
-    public function showViews();
-}
