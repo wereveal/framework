6b96f9306700e1eb4fdfa82f302766948a840ecc
William E Reveal (from Mac) <bill@revealitconsulting.com>
2013-07-31 14:49:10 -0500

    Clean Up

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 ob_start();
 $rodb      = false;
 $allow_get = true;
-require_once $_SERVER["DOCUMENT_ROOT"] . '/../app/setup.php';
-$o_guide   = new Wer\Guide\Controller\MainController;
-$html      = $o_guide->renderPage();
+require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/app/setup.php';
+$o_module   = new Ritc\Module\Controller\MainController;
+$html      = $o_module->renderPage();
 $any_junk  = ob_get_clean();
 ob_start();
     print $html;
