7a9ed109b8b862aac344cd7e36a39da1acf6925e
William E Reveal (from Mac) <wer@wereveal.com>
2013-11-07 11:02:27 -0600

    setup file for use of the framework on the command line

---
+++ app/cli_setup.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ *  This file sets up the Ritc Framework when doing things at the command line
+ *  Required to get the entire framework to work.
+ *  @file cli_setup.php
+ *  @namespace Ritc
+ *  @defgroup ritc_framework
+ *  @{
+ *      Previously named sitemanager. Was cut down to be just a basic framework
+ *      @version 4.0
+ *      @defgroup configs Configuration files
+ *      @ingroup ritc_framework
+ *      @defgroup core The core framework files
+ *      @ingroup ritc_framework
+ *  }
+ *  @defgroup main_app_name
+ *  @{
+ *      @version 1.0
+ *      @defgroup controllers controller files
+ *      @ingroup main_app_name
+ *      @defgroup views classes that create views
+ *      @ingroup main_app_name
+ *      @defgroup forms files that define and create forms
+ *      @ingroup views
+ *      @defgroup model files that do database operations
+ *      @ingroup main_app_name
+ *      @defgroup tests unitTesting
+ *      @ingroup main_app_name
+ *  }
+ *  @note <pre>
+ *  NOTE: _path and _PATH indicates a full server path
+ *        _dir and _DIR indicates the path in the site (URI)
+ *        Both do not end with a slash
+ *  </pre>
+*/
+namespace Ritc;
+
+use Ritc\Library\Core\Config;
+use Ritc\Library\Core\DbFactory;
+
+if (!defined('SITE_PATH')) {
+    define('SITE_PATH', __DIR__);
+}
+if (!defined('BASE_PATH')) {
+    define('BASE_PATH', dirname(SITE_PATH));
+}
+if (!defined('APP_PATH')) {
+    define('APP_PATH', BASE_PATH . '/app');
+}
+if (!defined('VENDOR_PATH')) {
+    define('VENDOR_PATH', BASE_PATH . '/vendor');
+}
+
+$loader = require_once VENDOR_PATH . '/autoload.php';
+$my_classmap = require_once APP_PATH . '/config/autoload_classmap.php';
+$loader->addClassMap($my_classmap);
+
+$o_default_dbf = DbFactory::start('db_config.php', 'rw');
+$o_default_pdo = $o_default_dbf->connect();
+
+require_once APP_PATH . '/config/constants.php';
+if ($o_default_pdo !== false) {
+    $o_default_db = new Database($o_default_pdo);
+    if (!Config::start($o_default_db)) {
+        error_log("Couldn't create the constants\n\n");
+        require_once APP_PATH . '/config/fallback_constants.php';
+    }
+}
+else {
+    require_once APP_PATH . '/config/fallback_constants.php';
+}
