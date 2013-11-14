2c1cf270bd6c35f7614a05ca59732621de4ed8d6
William E Reveal <bill@revealitconsulting.com>
2013-11-13 22:18:31 -0600

    Library changes required a config file change

--- app/config/twig_config.php
+++ app/config/twig_config.php
@@ -1,10 +1,14 @@
 <?php
-$twig_library = APP_PATH . '/Library/resources/templates/twig';
+$library_twig = APP_PATH . '/Library/resources/templates/twig';
 return array(
-    'default_path'        => $twig_library,
+    'default_path'        => $library_twig,
     'additional_paths'    => array(
-        'tests'   => $twig_library . '/test',
-        'default' => $twig_library . '/main'
+        'ritc_default'  => $library_twig . '/default',
+        'ritc_elements' => $library_twig . '/elements',
+        'ritc_main'     => $library_twig . '/main',
+        'ritc_pages'    => $library_twig . '/pages',
+        'ritc_snippets' => $library_twig . '/snippets',
+        'ritc_tests'    => $library_twig . '/tests'
     ),
     'environment_options' => array(
         'cache' => APP_PATH . '/twig_cache',
