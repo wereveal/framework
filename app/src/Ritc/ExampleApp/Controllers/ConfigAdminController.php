5a98726ec8f330c47d73777b4fa8cbf74c0af2ec
William E Reveal (mb) <bill@revealitconsulting.com>
2014-01-17 20:43:51 -0600

    Application Config files changed/added

---
+++ app/src/Ritc/ExampleApp/Controllers/ConfigAdminController.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ *  @brief Controller for the Configuration page.
+ *  @file ConfigAdminController.php
+ *  @ingroup ftpadmin controllers
+ *  @namespace Ritc/FtpAdmin/Controllers
+ *  @class ConfigAdminController
+ *  @author William Reveal  <bill@revealitconsulting.com>
+ *  @version 0.1
+ *  @date 2013-12-12 13:04:04
+ *  @note A file in FtpAdmin v1 app
+ *  @note <pre><b>Change Log</b>
+ *      v0.1 - Initial version 2013-12-12
+ *  </pre>
+ *  @todo Add the session validation setup
+ *  @todo add to ritc framework example app or maybe even to the Library as an abstract or interface.
+**/
+namespace Ritc\FtpAdmin\Controllers;
+
+use Ritc\Library\Core\Elog;
+use Ritc\Library\Core\TwigFactory as Twig;
+use Ritc\Library\Helper\ViewHelper;
+use Ritc\FtpAdmin\Models\AppConfig;
+
+class ConfigAdminController implements ControllerInterface
+{
+    protected $a_actions;
+    protected $a_values;
+    private $o_elog;
+    private $o_config;
+    private $o_twig;
+    private $o_vhelp;
+
+    public function __construct(array $a_actions = array(), array $a_values = array())
+    {
+        $this->o_elog    = Elog::start();
+        $this->o_config  = new AppConfig;
+        $this->o_twig    = Twig::start('twig_config.php');
+        $this->o_vhelp   = new ViewHelper();
+        $this->a_actions = $a_actions;
+        $this->a_values  = $a_values;
+    }
+    /**
+     *  Routes the code to the appropriate methods and returns a string.
+     *  @param array $a_actions optional, the actions derived from the URL/Form
+     *  @param array $a_values optional, the values from a form
+     *  @return string html to be displayed.
+    **/
+    public function renderPage(array $a_actions = array(), array $a_values = array())
+    {
+        $main_action = isset($a_actions['action2']) ? $a_actions['action2'] : '';
+        $form_action = isset($a_values['form_action']) ? $a_values['form_action'] : '';
+        // Make sure only valid routes are followed
+            switch ($main_action) {
+                case 'modify':
+                    switch ($form_action) {
+                        case 'verify':
+                            $main_action = 'verify';
+                            break;
+                        case 'update':
+                            $main_action = 'update';
+                            break;
+                        default:
+                            $main_action = '';
+                    }
+                    break;
+                case 'save':
+                    if ($form_action == 'save_new') {
+                        $main_action = 'save_new';
+                    }
+                    else {
+                        $main_action = '';
+                    }
+                    break;
+                case 'delete':
+                    if ($form_action == 'delete') {
+                        $main_action = 'delete';
+                    }
+                    else {
+                        $main_action = '';
+                    }
+                    break;
+                default:
+                    $main_action = '';
+            }
+
+        switch ($main_action) {
+            case 'save_new':
+                // save the record
+                $a_config = $a_values['config'];
+                $this->o_elog->write('config values before create config' . var_export($a_config, TRUE), LOG_OFF, __METHOD__ . '.' . __LINE__);
+                $results = $this->o_config->createConfig($a_config);
+                if ($results === false) {
+                    $a_message = array('type' => 'failure', 'message' => 'Could not save the configuration values.');
+                }
+                else {
+                    $a_message = array('type' => 'success', 'message' => 'Success!');
+                }
+                return $this->renderConfigs($a_message);
+            case 'update':
+                // save the record
+                $a_config = $a_values['config'];
+                $results = $this->o_config->updateConfig($a_config);
+                if ($results === false) {
+                    $a_message = array('type' => 'failure', 'message' => 'Could not update the configuration.');
+                }
+                else {
+                    $a_message = array('type' => 'success', 'message' => 'Success!');
+                }
+                return $this->renderConfigs($a_message);
+            case 'verify':
+                return $this->o_twig->render('@pages/verify_delete_config.twig', $a_values);
+            case 'delete':
+                // delete the record
+                $results = $this->o_config->deleteConfig($a_values['config_id']);
+                // $results = false;
+                if ($results === false) {
+                    $a_message = array('type' => 'failure', 'message' => 'Could not delete the configuration.');
+                }
+                else {
+                    $a_message = array('type' => 'success', 'message' => 'Success!');
+                }
+                return $this->renderConfigs($a_message);
+            default:
+                return $this->renderConfigs();
+        }
+    }
+
+    /**
+     *  Returns the list of configs in html.
+     *  @param array $a_message
+     *  @return string
+    **/
+    protected function renderConfigs(array $a_message = array()){
+        $a_values = array(
+            'a_message' => array(),
+            'a_configs' => array(
+                array(
+                    'config_id'    => '',
+                    'config_name'  => '',
+                    'config_value' => ''
+                )
+            ),
+            'tolken'  => $_SESSION['token'],
+            'form_ts' => $_SESSION['idle_timestamp'],
+            'hobbit'  => ''
+        );
+        if ($a_message != array()) {
+            $a_values['a_message'] = $this->o_vhelp->messageProperties($a_message);
+        }
+        else {
+            $a_values['a_message'] = $this->o_vhelp->messageProperties(
+                array(
+                    'message'       => 'Changing configuration values can result in unexpected results. If you are not sure, do not do it.',
+                    'type'          => 'warning'
+                )
+            );
+        }
+        $a_configs = $this->o_config->readConfig();
+        $this->o_elog->write('a_configs: ' . var_export($a_configs, TRUE), LOG_OFF, __METHOD__ . '.' . __LINE__);
+        if ($a_configs !== false && count($a_configs) > 0) {
+            $a_values['a_configs'] = $a_configs;
+        }
+        return $this->o_twig->render('@pages/app_config.twig', $a_values);
+    }
+}
--- app/src/Ritc/ExampleApp/Model/AppConfigEntity.php
+++ app/src/Ritc/ExampleApp/Model/AppConfigEntity.php
@@ -1,20 +1,20 @@
 <?php
 /**
- *  @brief Creates a entity object for the app configuration.
- *  @file ConfigEntity.php
- *  @ingroup example_app models
- *  @namespace Ritc/ExampleApp/Models
- *  @class ConfigEntity
+ *  @brief Creates a entity object.
+ *  @file AppConfigEntity.php
+ *  @ingroup ftpadmin models
+ *  @namespace Ritc/FtpAdmin/Models
+ *  @class AppConfigEntity
  *  @author William Reveal  <bill@revealitconsulting.com>
  *  @version 0.1.0
  *  @date 2013-12-12 13:21:16
- *  @note A file in the Ritc ExampleApp version 1.0
+ *  @note A file in the Ritc FtpAdmin version 1.0
  *  @note <b>SQL for table<b>
  *  <pre>
  *  CREATE TABLE `app_config` (
  *    `config_id` int(11) NOT NULL AUTO_INCREMENT,
- *    `config_name` varchar(64) CHARACTER SET latin1 NOT NULL,
- *    `config_value` varchar(200) CHARACTER SET latin1 NOT NULL,
+ *    `config_name` varchar(64) NOT NULL,
+ *    `config_value` varchar(64) NOT NULL,
  *    PRIMARY KEY (`config_id`),
  *    UNIQUE KEY `config_name` (`config_name`)
  *  ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4
@@ -23,35 +23,20 @@
  *  CREATE TABLE app_config (
  *      config_id integer DEFAULT nextval('app_config_id_seq'::regclass) NOT NULL,
  *      config_name character varying(64) NOT NULL,
- *      config_value character varying(200) NOT NULL
+ *      config_value character varying(64) NOT NULL
  *  )
  *  </pre>
  *  <pre>
  *  ALTER TABLE ONLY app_config
  *      ADD CONSTRAINT app_config_pkey PRIMARY KEY (config_id);
  *  </pre>
- *  <pre>
- *  INSERT INTO app_config (config_id, config_name, config_value)
- *  VALUES
- *      ( 1, 'DISPLAY_DATE_FORMAT', 'm/d/Y'),
- *      ( 2, 'EMAIL_DOMAIN', 'revealitconsulting.com'),
- *      ( 3, 'EMAIL_FORM_TO', 'bill@revealitconsulting.com'),
- *      ( 4, 'ERROR_EMAIL_ADDRESS', 'webmaster@revealitconsulting.com'),
- *      ( 5, 'PAGE_META_DESCRIPTION', 'Reveal IT Consulting'),
- *      ( 6, 'PAGE_META_KEYWORDS', 'Reveal IT Consulting'),
- *      ( 7, 'PAGE_TEMPLATE', 'index.twig'),
- *      ( 8, 'PAGE_TITLE', 'Reveal IT Consulting'),
- *      ( 9, 'THEME_NAME', 'default'),
- *      (10, 'ADMIN_THEME_NAME', 'default');
- *  </pre>
  *  @note <pre><b>Change Log</b>
  *      v0.1.0 - Initial version 12/12/2013</pre>
+ *  @todo add this to the ritc framework example app or maybe to the Library
 **/
-namespace Ritc\ExampleApp\Models;
+namespace Ritc\FtpAdmin\Models;
 
-use Ritc\Library\Interfaces\EntityInterface;
-
-class ConfigEntity implements EntityInterface
+class AppConfigEntity implements namespace\EntityInterface
 {
     private $config_id;
     private $config_name;
@@ -61,7 +46,7 @@ class ConfigEntity implements EntityInterface
     {
         $this->config_id = $value;
     }
-    public function setName($value = '')
+    public function setKey($value = '')
     {
         $this->config_name = $value;
     }
@@ -73,7 +58,7 @@ class ConfigEntity implements EntityInterface
     {
         return $this->config_id;
     }
-    public function getName()
+    public function getKey()
     {
         return $this->config_name;
     }
@@ -101,7 +86,7 @@ class ConfigEntity implements EntityInterface
     public function setAllProperties(array $a_entity = array())
     {
         $this->config_id    = isset($a_entity['config_id'])    ? $a_entity['config_id']    : '';
-        $this->config_name  = isset($a_entity['config_name'])  ? $a_entity['config_name']   : '';
+        $this->config_name  = isset($a_entity['config_name'])  ? $a_entity['config_name']  : '';
         $this->config_value = isset($a_entity['config_value']) ? $a_entity['config_value'] : '';
     }
 }
