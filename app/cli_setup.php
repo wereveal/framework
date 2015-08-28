945f0bfc2dfce365035358e6df522c5ca22c26ca
William E Reveal (from Mac) <bill@revealitconsulting.com>
2015-08-28 09:00:46 -0500

    change to match Library changes

--- app/cli_setup.php
+++ app/cli_setup.php
@@ -1,9 +1,8 @@
 <?php
 /**
- *  @brief This file sets up the App.
- *  @description Required to get the entire framework to work. The only thing
- *  that changes primarily is the defgroup in this comment for Doxygen.
- *  @file setup.php
+ *  This file sets up the app when doing things at the command line
+ *  Required to get the entire app to work.
+ *  @file cli_setup.php
  *  @namespace Ritc
  *  @defgroup ritc_library
  *  @{
@@ -31,32 +30,31 @@
  *      @defgroup views
  *      @ingroup ritc_library
  *  }
- *  @defgroup ftpadmin
+ *  @defgroup main_app_name
  *  @{
  *      @version 1.0
- *      @defgroup ftp_configs
- *      @ingroup ftpadmin
- *      @defgroup ftp_controllers controller files
- *      @ingroup ftpadmin
- *      @defgroup ftp_views classes that create views
- *      @ingroup ftpadmin
- *      @defgroup ftp_models files that do database operations
- *      @ingroup ftpadmin
- *      @defgroup ftp_tests unit Testing
- *      @ingroup ftpadmin
+ *      @defgroup controllers controller files
+ *      @ingroup main_app_name
+ *      @defgroup views classes that create views
+ *      @ingroup main_app_name
+ *      @defgroup forms files that define and create forms
+ *      @ingroup views
+ *      @defgroup model files that do database operations
+ *      @ingroup main_app_name
+ *      @defgroup tests unitTesting
+ *      @ingroup main_app_name
  *  }
  *  @note <pre>
  *  NOTE: _path and _PATH indicates a full server path
  *        _dir and _DIR indicates the path in the site (URI)
  *        Both do not end with a slash
  *  </pre>
- * @TODO Serious refactoring gone amiss. Bunch of the use statements point wrong.
- */
+*/
 namespace Ritc;
 
+use Ritc\Library\Helper\ConstantsHelper;
 use Ritc\Library\Factories\DbFactory;
 use Ritc\Library\Factories\TwigFactory;
-use Ritc\Library\Helper\ConstantsHelper;
 use Ritc\Library\Services\DbModel;
 use Ritc\Library\Services\Di;
 use Ritc\Library\Services\Elog;
@@ -67,18 +65,15 @@ if (!defined('SITE_PATH')) {
     define('SITE_PATH', __DIR__);
 }
 if (!defined('BASE_PATH')) {
-    if (!isset($app_in)) {
-        $app_in = 'external';
+    if (!isset($app_is_in)) {
+        $app_is_in = 'external';
     }
-    if ($app_in == 'site' || $app_in == 'htdocs' || $app_in == 'html') {
+    if ($app_is_in == 'site' || $app_is_in == 'htdocs' || $app_is_in == 'html') {
         define('BASE_PATH', SITE_PATH);
     }
     else {
         define('BASE_PATH', dirname(dirname(__FILE__)));
     }
-}
-if (!defined('DEVELOPER_MODE')) {
-    define('DEVELOPER_MODE', false);
 }
 if (!isset($rodb)) {
     $rodb = false;
@@ -86,27 +81,27 @@ if (!isset($rodb)) {
 
 require_once BASE_PATH . '/app/config/constants.php';
 
-$o_loader = require_once VENDOR_PATH . '/autoload.php';
-$my_classmap = require_once APP_CONFIG_PATH . '/autoload_classmap.php';
-$o_loader->addClassMap($my_classmap);
+$loader = require_once VENDOR_PATH . '/autoload.php';
+$my_classmap = require_once APP_PATH . '/config/autoload_classmap.php';
+$loader->addClassMap($my_classmap);
 
-$o_elog    = Elog::start();
+$o_elog = Elog::start();
+$o_elog->setIgnoreLogOff(false); // turns on logging globally ignoring LOG_OFF when set to true
+
 $o_session = Session::start();
-$o_di      = new Di();
+
+$o_di = new Di();
 $o_di->set('elog',    $o_elog);
 $o_di->set('session', $o_session);
-$use_local_db = false;
-if ($use_local_db) {
+
+if ($_SERVER['SERVER_NAME'] == 'w3.qca.net') {
     $db_config_file = 'db_config.php';
 }
 else {
     $db_config_file = 'db_local_config.php';
 }
-$o_dbf = DbFactory::start($db_config_file, 'rw');
-$o_dbf->setElog($o_elog);
-$o_elog->setIgnoreLogOff(false); // turns on logging globally ignoring LOG_OFF when set to true
 
-$o_pdo = $o_dbf->connect();
+$o_pdo = DbFactory::start($db_config_file, 'rw', $o_di);
 
 if ($o_pdo !== false) {
     $o_db = new DbModel($o_pdo, $db_config_file);
@@ -116,30 +111,32 @@ if ($o_pdo !== false) {
     }
     else {
         $o_di->set('db', $o_db);
-        if (!ConstantsHelper::start($o_di)) {
-            $o_elog->write("Couldn't create the constants\n", LOG_ALWAYS);
-            require_once APP_CONFIG_PATH . '/fallback_constants.php';
-        }
-        $a_constants = get_defined_constants(true);
-        $o_elog->write(var_export($a_constants['user'], true), LOG_OFF);
-        $o_router = new Router($o_di);
-        $o_tpl    = TwigFactory::getTwig('twig_config.php');
-        if ($rodb) {
-            $o_dbf_ro = DbFactory::start($db_config_file, 'ro');
-            $o_pdo_ro = $o_dbf_ro->connect();
+        if (RODB) {
+            $o_pdo_ro = DbFactory::start($db_config_file, 'ro');
             if ($o_pdo_ro !== false) {
                 $o_db_ro = new DbModel($o_pdo_ro, $db_config_file);
                 if (!is_object($o_db_ro)) {
                     $o_elog->write("Could not create a new DbModel for read only\n", LOG_ALWAYS);
                     die("Could not get the database to work");
                 }
-                $o_di->set('db', $o_db_ro);
-                $o_di->set('db_rw', $o_db);
+                $o_di->set('rodb', $o_db_ro);
             }
         }
-        $o_di->set('router', $o_router);
-        $o_di->set('tpl',    $o_tpl);
-        $o_di->set('twig',   $o_tpl); // tpl or twig, never sure which one
+
+        if (!ConstantsHelper::start($o_di)) {
+            $o_elog->write("Couldn't create the constants\n", LOG_ALWAYS);
+            require_once APP_CONFIG_PATH . '/fallback_constants.php';
+        }
+
+        /*
+        $a_constants = get_defined_constants(true);
+        $o_elog->write(var_export($a_constants['user'], true), LOG_OFF);
+        */
+
+        $o_router = new Router($o_di);
+        $o_twig   = TwigFactory::getTwig('twig_config.php');
+        $o_di->set('router',  $o_router);
+        $o_di->set('tpl',     $o_twig);
     }
 }
 else {
