d0f3f61cb07cb83b8afe419cc8eaded6884b6188
William E Reveal (from Mac) <bill@revealitconsulting.com>
2017-01-12 10:40:41 -0600

    Config changes.

--- app/config/autoload_namespaces.php
+++ app/config/autoload_namespaces.php
@@ -1,7 +1,6 @@
 <?php
-* Generated on 2016-03-07T13:20:19-06:00 by AutoloadMapper *
+/* Generated on 2017-01-10T12:25:22-06:00 by AutoloadMapper */
 return array(
-    'Ritc\\'    => __DIR__ . '/../src/Ritc',
-    'Example\\' => __DIR__ . '/../src/Example',
+    'Ritc\\' => __DIR__ . '/../src/Ritc',
 
 );
