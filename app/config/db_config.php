edd390a2590b51f0104a41f98ad50374b8ed1c91
William E Reveal (from Mac) <bill@revealitconsulting.com>
2017-01-15 23:35:26 -0600

    db_config file changed.  HOWEVER, this file probably should not be part of the git repo. Time to remove it after this commit.

--- app/config/db_config.php
+++ app/config/db_config.php
@@ -1,15 +1,14 @@
 <?php
-return array(
-    'driver'     => 'mysql',
-    'host'       => 'localhost',
-    'port'       => '',
-    'name'       => 'ritc',
-    'user'       => 'wer',
-    'password'   => 'bn2fak.DB',
-    'userro'     => 'wer',
-    'passro'     => 'bn2fak.DB',
-    'persist'    => false,
-    'prefix'     => 'ritc_',
-    'lib_prefix' => 'ritc_',
-    'ftp_prefix' => 'ftp_'
-);
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => '',
+    'name' => 'ritc',
+    'user' => 'wer',
+    'password' => 'bn2fak.DB',
+    'userro' => 'wer',
+    'passro' => 'bn2fak.DB',
+    'persist' => '',
+    'prefix' => 'ritc_',
+    'lib_prefix' => 'ritc_'
+];
