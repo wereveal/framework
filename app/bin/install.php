3f7694ce9343247d1faa3107222f76eda06b31b3
William E Reveal (from Mac) <bill@revealitconsulting.com>
2017-01-26 11:48:03 -0600

    More changes to the way install works.

--- app/bin/install.php
+++ app/bin/install.php
@@ -542,7 +542,7 @@ $app_path = SRC_PATH . '/' . $a_install['namespace'] . '/' . $a_install['app_nam
 $a_new_dirs = ['Abstracts', 'Controllers', 'Entities', 'Interfaces', 'Models',
 'Tests', 'Traits', 'Views', 'resources', 'resources/config', 'resources/sql',
 'resources/templates', 'resources/themes', 'resources/templates/default',
-'resources/templates/elements', 'resources/templates/pages',
+'resources/templates/elements', 'resources/templates/pages', 'resources/templates/forms',
 'resources/templates/snippets', 'resources/templates/tests'];
 
 $htaccess_text =<<<EOF
@@ -578,35 +578,68 @@ if (!file_exists($app_path)) {
     }
 }
 
+$a_find = [
+    '{NAMESPACE}',
+    '{APPNAME}',
+    '{namespace}',
+    '{app_name}',
+    '{author}',
+    '{sauthor}',
+    '{email}',
+    '{idate}',
+    '{sdate}',
+    '{twig_prefix}'
+];
+$a_replace = [
+    $a_install['namespace'],
+    $a_install['app_name'],
+    strtolower($a_install['namespace']),
+    strtolower($a_install['app_name']),
+    $a_install['author'],
+    $a_install['short_author'],
+    $a_install['email'],
+    date('Y-m-d H:i:s'),
+    date('Y-m-d'),
+    $a_install['twig_prefix']
+];
+
 ### Create the main controller for the app ###
 print "Creating the main controller for the app\n";
 $controller_text = file_get_contents(APP_CONFIG_PATH . '/install/main_controller.txt');
-$controller_text = str_replace('{NAMESPACE}', $a_install['namespace'], $controller_text);
-$controller_text = str_replace('{APPNAME}', $a_install['app_name'], $controller_text);
-$group_name = strtolower($a_install['app_name']) . '_controllers';
-$controller_text = str_replace('{GROUPNAME}', $group_name, $controller_text);
-file_put_contents($app_path . '/Controllers/MainController.php', $controller_text);
+$controller_text = str_replace($a_find, $a_replace, $controller_text);
+file_put_contents($app_path . '/Controllers/FtpController.php', $controller_text);
+
+### Create the main view for the app ###
+print "Creating the main view for the app\n";
+$view_text = file_get_contents(APP_CONFIG_PATH . '/install/home_view.txt');
+$view_text = str_replace($a_find, $a_replace, $view_text);
+file_put_contents($app_path . '/Views/HomeView.php', $view_text);
 
 ### Create the doxygen config for the app ###
 print "Creating the doxy config for the app\n";
 $doxy_text = file_get_contents(APP_CONFIG_PATH . '/install/doxygen_config.txt');
-$namespace = strtolower($a_install['namespace']);
-$app_name  = strtolower($a_install['app_name']);
-$doxy_text = str_replace('{NAMESPACE}', $namespace, $doxy_text);
-$doxy_text = str_replace('{APPNAME}', $app_name, $doxy_text);
+$doxy_text = str_replace($a_find, $a_replace, $doxy_text);
 file_put_contents($app_path . '/resources/config/doxygen_config.php', $doxy_text);
 
 ### Create the twig_config file ###
 print "Creating the twig config file for app\n";
-$twig_file = file_get_contents(APP_CONFIG_PATH . '/install/twig_config.php');
-$new_twig_file = str_replace('REPLACE_ME',  "{$a_install['namespace']}/{$a_install['app_name']}/resources/templates", $twig_file);
+$twig_file = file_get_contents(APP_CONFIG_PATH . '/install/twig_config.txt');
+$new_twig_file = str_replace($a_find, $a_replace, $twig_file);
 file_put_contents(APP_CONFIG_PATH . '/twig_config.php', $new_twig_file);
+
+### Copy two main twig files ###
+print "Copying twig files\n";
+$first_file = '/resources/templates/default/base.twig';
+$second_file = '/resources/templates/pages/index.twig';
+$twig_text = file_get_contents(LIBRARY_PATH . $first_file);
+file_put_contents($app_path . $first_file, $twig_text);
+$twig_text = file_get_contents(LIBRARY_PATH . $second_file);
+file_put_contents($app_path . $second_file, $twig_text);
 
 ### Create the index.php file ###
 print "Creating the index.php file for app\n";
 $index_text = file_get_contents(APP_CONFIG_PATH . '/install/index.php.txt');
-$index_text = str_replace('{NAMESPACE}', $a_install['namespace'], $index_text);
-$index_text = str_replace('{APPNAME}', $a_install['app_name'], $index_text);
+$index_text = str_replace($a_find, $a_replace, $index_text);
 file_put_contents(SITE_PATH . '/index.php', $index_text);
 
 ### Regenerate Autoload Map files
--- app/config/install/install_config.php
+++ app/config/install/install_config.php
@@ -3,6 +3,9 @@
 return [
     'app_name'        => 'App',
     'namespace'       => 'Ritc',
+    'author'          => 'William E Reveal',
+    'short_author'    => 'wer',
+    'email'           => '<bill@revealitconsulting.com>',
     'db_file'         => 'db_config.php',
     'db_host'         => 'localhost',
     'db_type'         => 'mysql',
@@ -12,7 +15,7 @@ return [
     'db_pass'         => 'dbpass',
     'db_persist'      => false,
     'db_prefix'       => 'ritc_',
-    'lib_db_prefix'   => 'lib_',
+    'lib_db_prefix'   => 'ritc_',
     'twig_prefix'     => 'app_',
     'lib_twig_prefix' => 'lib_',
     'loader'          => 'psr4',
