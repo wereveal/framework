3617d4e44b8b7749e2527033d0596f6ef27cbaee
William E Reveal (from Mac) <bill@revealitconsulting.com>
2017-03-10 10:49:19 -0600

    A pseudo renaming of Ritc/App to Ritc/Main by deleting Ritc/App and replacing it with the ritcweb app.

---
+++ src/apps/Ritc/Main/resources/config/doxygen_config.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ *  @defgroup ritc_main
+ *  @ingroup ritc
+ *  @{
+ *      @namespace Ritc\Main
+ *      @version 1.0
+ *      @defgroup main_abstracts Abstract class files
+ *      @ingroup ritc_main
+ *      @defgroup main_controllers Controller files
+ *      @ingroup ritc_main
+ *      @defgroup main_entities Defines the tables in the database
+ *      @ingroup ritc_main
+ *      @defgroup main_interfaces Files that define what a class should have
+ *      @ingroup ritc_main
+ *      @defgroup main_models Classes that do database calls
+ *      @ingroup ritc_main
+ *      @defgroup main_tests Classes that test other classes
+ *      @ingroup ritc_main
+ *      @defgroup main_traits Functions that are common to multiple classes
+ *      @ingroup ritc_main
+ *      @defgroup main_views Classes that provide the end user experience
+ *      @ingroup ritc_main
+ *  }
+ */
