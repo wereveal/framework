600cd995aced4760a3f9129b081b0668a1bf61b0
William E Reveal (from mb) <bill@revealitconsulting.com>
2018-05-24 16:02:54 -0500

    updated skeleton script

--- src/bin/skeleton_script.php
+++ src/bin/skeleton_script.php
@@ -6,13 +6,16 @@
  * @author    William E Reveal <bill@revealitconsulting.com>
  * @date      2017-02-08 08:40:47
  * @version   1.0.0
- * @note   <b>Change Log</b><pre>
- *  v1.0.0 - initial version                                      - 2017-02-08 wer
- * </pre>
+ * @note      Change Log
+ * - v1.1.0 - updated for exceptions
+ * - v1.0.0 - initial version                                      - 2017-02-08 wer
  */
 namespace Ritc;
 
+use Ritc\Library\Exceptions\FactoryException;
+use Ritc\Library\Exceptions\ServiceException;
 use Ritc\Library\Factories\PdoFactory;
+use Ritc\Library\Helper\AutoloadMapper;
 use Ritc\Library\Services\DbModel;
 use Ritc\Library\Services\Di;
 use Ritc\Library\Services\Elog;
@@ -28,6 +31,20 @@ if (!file_exists(APPS_PATH . '/Ritc/Library')) {
     die("You must clone the Ritc/Library in the apps dir first and any other desired apps.\n");
 }
 
+if (!file_exists(SRC_CONFIG_PATH . '/autoload_namespaces.php')) {
+    require APPS_PATH . '/Ritc/Library/Helper/AutoloadMapper.php';
+    $a_dirs = [
+        'src_path'    => SRC_PATH,
+        'config_path' => SRC_CONFIG_PATH,
+        'apps_path'   => APPS_PATH
+    ];
+    $o_cm = new AutoloadMapper($a_dirs);
+    if (!is_object($o_cm)) {
+        die("Could not instance AutoloadMapper");
+    }
+    $o_cm->generateMapFiles();
+
+}
 $o_loader = require_once VENDOR_PATH . '/autoload.php';
 
 $my_namespaces = require_once SRC_CONFIG_PATH . '/autoload_namespaces.php';
@@ -35,15 +52,26 @@ foreach ($my_namespaces as $psr4_prefix => $psr0_paths) {
     $o_loader->addPsr4($psr4_prefix, $psr0_paths);
 }
 
-$o_elog = Elog::start();
-$o_elog->setErrorHandler(E_USER_WARNING | E_USER_NOTICE | E_USER_ERROR);
-$o_elog->setIgnoreLogOff(true); // turns on logging globally ignoring LOG_OFF when set to true
-$o_elog->write("Test\n", LOG_OFF);
+try {
+    $o_elog = Elog::start();
+    $o_elog->setErrorHandler(E_USER_WARNING | E_USER_NOTICE | E_USER_ERROR);
+    $o_elog->setIgnoreLogOff(true); // turns on logging globally ignoring LOG_OFF when set to true
+    $o_elog->write("Test\n", LOG_OFF);
+}
+catch (ServiceException $e) {
+    die("Unable to start Elog" . $e->errorMessage());
+}
+
 $o_di = new Di();
 $o_di->set('elog', $o_elog);
 
 $db_config_file = SRC_CONFIG_PATH . '/db_config_local.php';
-$o_pdo = PdoFactory::start($db_config_file, 'rw', $o_di);
+try {
+    $o_pdo = PdoFactory::start($db_config_file, 'rw', $o_di);
+}
+catch (FactoryException $e) {
+    die("Unable to start the PdoFactory. " . $e->errorMessage());
+}
 
 if ($o_pdo !== false) {
     $o_db = new DbModel($o_pdo, $db_config_file);
