611cc86d881ae011fbefd8c315215cdfeb6f7403
William E Reveal (from Mac) <bill@revealitconsulting.com>
2018-06-26 16:17:05 -0500

    bug fixes

--- src/bin/doSass.sh
+++ src/bin/doSass.sh
@@ -12,3 +12,19 @@ else
   fi
 fi
 sass --update -s compressed ${theDir}:${theOtherDir}
+
+if [ -d src/apps/Ritc/Library/resources/assets/scss/ ]; then
+  theDir='src/apps/Ritc/Library/resources/assets/scss/'
+  theOtherDir='public/assets/css/'
+else
+  if [ -d ../apps/Ritc/Library/resources/assets/scss/ ]; then
+    theDir='../apps/Ritc/Library/resources/assets/scss/'
+    theOtherDir='../../public/assets/css/'
+  else
+    theDir=''
+    theOtherDir=''
+  fi
+fi
+if [[ ${theDir} != "" ]]; then
+  sass --update -s compressed ${theDir}:${theOtherDir}
+fi
--- src/bin/doUglifyJS.sh
+++ src/bin/doUglifyJS.sh
@@ -17,3 +17,23 @@ do
     shortThing=$(basename ${thing})
     uglifyjs ${thing} --compress --mangle --output ${theOtherDir}${shortThing}
 done
+
+if [ -d src/apps/Ritc/Library/resources/assets/js/ ]; then
+  theDir='src/apps/Ritc/Library/resources/assets/js/'
+  theOtherDir='public/assets/js/'
+else
+  if [ -d ../apps/Ritc/Library/resources/assets/js/ ]; then
+    theDir='../apps/Ritc/Library/resources/assets/js/'
+    theOtherDir='../../public/assets/js/'
+  else
+    theDir=''
+    theOtherDir='min/'
+  fi
+fi
+if [[ ${theDir} != '' ]]; then
+for thing in $(ls ${theDir}*.js)
+do
+    shortThing=$(basename ${thing})
+    uglifyjs ${thing} --compress --mangle --output ${theOtherDir}${shortThing}
+done
+fi
