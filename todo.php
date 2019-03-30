36dad2eca8fbad4a44f4768f32da4601ba075110
William E Reveal (from Mac) <bill@revealitconsulting.com>
2019-03-30 03:00:09 -0500

    Updated twig tpls to use default twig shortcuts to files.

--- todo.php
+++ todo.php
@@ -1,6 +1,4 @@
 <?php
 /*
- * Working on multiple themes
- * At this point, each theme will have its own twig prefix and associated twig dirs and twig tpls
- * However, there is nothing to prevent the themes to share directories, KEY!
+ * Test all changes to default templates to use the new twig shortcuts.
 */
