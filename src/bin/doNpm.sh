5264cfd4a755a7956d76e923e3a8a8a7dd5edd38
William E Reveal (from Mac) <bill@revealitconsulting.com>
2021-12-01 23:40:34 -0600

    updating files for the initial install

--- src/bin/doNpm.sh
+++ src/bin/doNpm.sh
@@ -8,6 +8,11 @@ else
     theDir=''
   fi
 fi
+if [ ! -f $theDir/package.json ]; then
+    if [ -f ../config/install_files/package.json.bulma.txt ]; then
+        cp ../config/install_files/package.json.bulma.txt $theDir/
+    fi
+fi
 cd $theDir
 npm install
 if [ ! -d vendor ]; then
